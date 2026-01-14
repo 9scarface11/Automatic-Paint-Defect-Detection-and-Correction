351531e0e2a325b268a259148e216f42adc563f2
9scarface11 <rathoremehul07@gmail.com>
2026-01-14 21:09:16 +0530

    Update download_model.py

--- CNN/scripts/download_model.py
+++ CNN/scripts/download_model.py
@@ -6,7 +6,8 @@ MODEL_PATH = os.path.join(MODEL_DIR, "model.pth")
 
 os.makedirs(MODEL_DIR, exist_ok=True)
 
-url = "https://drive.google.com/file/d/1M6cGRCVpzDU9zKENnznXUWDan83yCRak/view?usp=sharing"
+url = "https://drive.google.com/file/d/1p0pg8z0B3Gg1-_TyHjHGuCbnwTbfAhsY/view?usp=drive_link"
 gdown.download(url, MODEL_PATH, quiet=False)
 
 print("Model downloaded successfully.")
+
